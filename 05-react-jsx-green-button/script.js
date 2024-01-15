b6acf7736c6484e2585edc36cf96b5a28f26e897
uladzimir <01myit@gmail.com>
2024-01-15 19:27:29 +0300

    Created React component and used it.

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,16 +1,13 @@
-const DivWithButton = (
-  <div className='app'>
-    <button
-      // id='our-btn'
-      onClick={(e) => changeButton(e)}
-    >
-      Hello JSX!
-    </button>
-  </div>
-)
-function changeButton(e) {
-  // document.getElementById('our-btn').className = 'btn-green'
-  e.target.className = 'btn-green'
-  e.target.innerText = 'The text was changed!'
+const DivWithButton = () => {
+  function changeButton(e) {
+    e.target.className = 'btn-green'
+    e.target.innerText = 'The text was changed!'
+  }
+  return (
+    <div className='app'>
+      <button onClick={(e) => changeButton(e)}>Hello JSX!</button>
+    </div>
+  )
 }
-ReactDOM.createRoot(document.querySelector('#root')).render(DivWithButton)
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<DivWithButton />)
