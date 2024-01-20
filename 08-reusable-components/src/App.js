e30578215725c954de0c27a6e0d367bb4b2ec557
uladzimir <01myit@gmail.com>
2024-01-20 14:51:04 +0300

    Added a new component.

--- 08-reusable-components/src/App.js
+++ 08-reusable-components/src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
 import MyComponent from './components/MyComponent'
+import OtherComponent from './components/OtherComponent'
+
 function App() {
   return (
     <div className='App'>
       <MyComponent />
-      <MyComponent />
-      <MyComponent />
-      <MyComponent />
+      <OtherComponent />
     </div>
   )
 }
---
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,13 @@
+const OtherComponent = () => {
+  return (
+    <div>
+      <p>
+        In the world of software management there exists a dreaded place called
+        “dependency hell.” The bigger your system grows and the more packages
+        you integrate into your software, the more likely you are to find
+        yourself, one day, in this pit of despair.
+      </p>
+    </div>
+  )
+}
+export default OtherComponent
