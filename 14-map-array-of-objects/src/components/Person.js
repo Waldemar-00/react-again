7f3fd9abe2ed8fdf0b23b2810b83283df0463e61
uladzimir <01myit@gmail.com>
2024-01-22 17:40:35 +0300

    Changed the data, changed CSS styles and JSX of the Person component.

--- 14-map-array-of-objects/src/components/Person.js
+++ 14-map-array-of-objects/src/components/Person.js
@@ -2,14 +2,14 @@ import './Person.css'
 function Person({ firstName, lastName, email, img }) {
   return (
     <section>
+      <img
+        src={img}
+        alt='person'
+      />
       <h2>
         {firstName} {lastName}
       </h2>
       <h3>{email}</h3>
-      <img
-        src={`${img}`}
-        alt='person'
-      />
     </section>
   )
 }
