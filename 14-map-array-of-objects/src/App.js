d72f1257d4f790b1c56106221245f43795685099
uladzimir <01myit@gmail.com>
2024-01-22 16:47:25 +0300

    Used the  user data in the Person component.

--- 14-map-array-of-objects/src/App.js
+++ 14-map-array-of-objects/src/App.js
@@ -1,9 +1,21 @@
 import './App.css'
 import Person from './components/Person'
+import { persons } from './data/persons'
 function App() {
   return (
     <div className='App'>
-      <Person />
+      {persons.map((person) => {
+        const { id, firstName, lastName, email, img } = person
+        return (
+          <Person
+            key={id}
+            firstName={firstName}
+            lastName={lastName}
+            email={email}
+            img={img}
+          />
+        )
+      })}
     </div>
   )
 }
--- 14-map-array-of-objects/src/components/Person.js
+++ 14-map-array-of-objects/src/components/Person.js
@@ -1,5 +1,13 @@
 import './Person.css'
-function Person() {
-  return <div>Person</div>
+function Person({ firstName, lastName, email, img }) {
+  return (
+    <section>
+      <div>{firstName}</div>
+      <div>{lastName}</div>
+      <div>{email}</div>
+      <div>{img}</div>
+      <hr />
+    </section>
+  )
 }
 export default Person
