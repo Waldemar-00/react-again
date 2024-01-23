d54563f2d20dd5478b5e35984b890b47fa36add3
uladzimir <01myit@gmail.com>
2024-01-23 14:29:42 +0300

    Used the React Fragment without import the  React.

--- 16-react-fragment/src/components/Card.js
+++ 16-react-fragment/src/components/Card.js
@@ -1,11 +1,10 @@
-import React from 'react'
 function Card() {
   return (
-    <React.Fragment>
+    <>
       <h1>The React learning</h1>
       <h2>The Fragment like a part of the React</h2>
       <button type='button'>Like!</button>
-    </React.Fragment>
+    </>
   )
 }
 export default Card
