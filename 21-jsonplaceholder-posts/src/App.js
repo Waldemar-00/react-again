3faa4c62ef0ee744cd4ff542a6ce448c6e51bf89
uladzimir <01myit@gmail.com>
2024-01-25 12:38:16 +0300

    Catched errors, renamed the post variable to item.

--- 21-jsonplaceholder-posts/src/App.js
+++ 21-jsonplaceholder-posts/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className='App'>
       <h1 style={{ color: 'yellowgreen' }}>ALL POST from JSONPLACEHOLDER</h1>
       <Posts />
+      <h2 style={{ color: 'yellowgreen' }}>FOOTER</h2>
     </div>
   )
 }
--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -7,15 +7,17 @@ function Posts() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((json) => setPosts(json))
+      .catch((error) => console.error(error))
   }, [])
   return (
     <section>
-      {posts &&
-        posts.map((post) => {
+      {Array.isArray(posts) &&
+        posts.map((item) => {
           return (
             <Post
-              title={post.title}
-              body={post.body}
+              key={item.id}
+              title={item.title}
+              body={item.body}
             />
           )
         })}
