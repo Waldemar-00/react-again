b77d159884f27fcbddba1d1bf8936d7b362aeca3
uladzimir <01myit@gmail.com>
2024-01-25 16:55:08 +0300

    Added the error block.

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -2,24 +2,25 @@ import React from 'react'
 import Post from './Post'
 
 function Posts() {
-  const [posts, setPosts] = React.useState(null)
+  const [posts, setPosts] = React.useState([])
+  const [error, setError] = React.useState('')
+
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((json) => setPosts(json))
-      .catch((error) => console.error(error))
+      .catch((error) => setError(error.message))
   }, [])
+  if(error) return <h2 style={{ color: 'red' }}>Error: {error}</h2>
   return (
     <section>
-      {Array.isArray(posts) &&
-        posts.map((item) => {
-          return (
-            <Post
-              key={item.id}
-              {...item}
-            />
-          )
-        })}
+      {(Array.isArray(posts) &&
+        posts.map((item) => (
+          <Post
+            key={item.id}
+            {...item}
+          />
+        )))}
     </section>
   )
 }
