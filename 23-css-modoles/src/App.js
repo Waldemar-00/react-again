57c32aed902b1eaa1a340757953f03d27726109f
uladzimir <01myit@gmail.com>
2024-01-26 15:42:07 +0300

    CSS modules.

--- 23-css-modoles/src/App.js
+++ 23-css-modoles/src/App.js
@@ -3,8 +3,12 @@ import Info from './components/info'
 function App() {
   return (
     <div className='App'>
-      <h1>This is the App component</h1>
-      <button className='btn-Info'>Click the App!</button>
+      {/* for example: div with className from CSS module Info - not work the css styles! */}
+      <div className='info'>
+        <h1>This is the App component</h1>
+        <h3>For example</h3>
+        <button className='btn-Info'>Click the App!</button>
+      </div>
       <Info />
     </div>
   )
--- 23-css-modoles/src/components/info.js
+++ 23-css-modoles/src/components/info.js
@@ -1,10 +1,18 @@
 import './Info.css'
+import styles from './Info.module.css'
+// {
+//   "info": "Info_info__anGgR",
+//   "btnInfo": "Info_btnInfo__fS785",
+//   "h2": "Info_h2__QJYx0"
+// }
+console.log(styles)
 function Info() {
   return (
-    <>
-      <h1>This is the Info component</h1>
-      <button className='btn-Info'>Click the Info!</button>
-    </>
+    <div className={styles.info}>
+      <h2 className={styles.h2}>This is the Info component</h2>
+      <h3>For exapmple</h3>
+      <button className={styles.btnInfo}>Click the Info!</button>
+    </div>
   )
 }
 export default Info
