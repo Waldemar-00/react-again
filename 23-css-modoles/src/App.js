815d8257ad837d5e8fe0ae4fa6281534380be147
uladzimir <01myit@gmail.com>
2024-01-26 15:04:41 +0300

    Common styles.

---
+++ 23-css-modoles/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Info from './components/info'
+function App() {
+  return (
+    <div className='App'>
+      <h1>This is the App component</h1>
+      <button className='btn-Info'>Click the App!</button>
+      <Info />
+    </div>
+  )
+}
+
+export default App
---
+++ 23-css-modoles/src/components/info.js
@@ -0,0 +1,10 @@
+import './Info.css'
+function Info() {
+  return (
+    <>
+      <h1>This is the Info component</h1>
+      <button className='btn-Info'>Click the Info!</button>
+    </>
+  )
+}
+export default Info
