fab067f36e7a90b9537eaf0c1cc23cef2354e8ed
uladzimir <01myit@gmail.com>
2024-01-26 12:36:57 +0300

    Changed logical of the status Loading...

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -4,19 +4,14 @@ import Post from './Post'
 function Posts() {
   const [posts, setPosts] = React.useState([])
   const [error, setError] = React.useState('')
-  const [isLoading, setLoading] = React.useState(false)
+  const [isLoading, setLoading] = React.useState(true)
 
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        setLoading(true)
-        return response.json()
-      })
-      .then((json) => {
-        setPosts(json)
-        setLoading(false)
-      })
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
       .catch((error) => setError(error.message))
+      .finally(() => setLoading(false))
   }, [])
   if (error) return <h2 style={{ color: 'red' }}>Error: {error}</h2>
   if (isLoading) return <h2 style={{ color: 'lightblue' }}>Loading...</h2>
