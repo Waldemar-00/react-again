a2e54be712d06e1bf38be98e666440fa97645cc4
uladzimir <01myit@gmail.com>
2024-01-26 12:28:58 +0300

    Added status Loading...

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -4,23 +4,31 @@ import Post from './Post'
 function Posts() {
   const [posts, setPosts] = React.useState([])
   const [error, setError] = React.useState('')
+  const [isLoading, setLoading] = React.useState(false)
 
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((json) => setPosts(json))
+      .then((response) => {
+        setLoading(true)
+        return response.json()
+      })
+      .then((json) => {
+        setPosts(json)
+        setLoading(false)
+      })
       .catch((error) => setError(error.message))
   }, [])
-  if(error) return <h2 style={{ color: 'red' }}>Error: {error}</h2>
+  if (error) return <h2 style={{ color: 'red' }}>Error: {error}</h2>
+  if (isLoading) return <h2 style={{ color: 'lightblue' }}>Loading...</h2>
   return (
     <section>
-      {(Array.isArray(posts) &&
+      {Array.isArray(posts) &&
         posts.map((item) => (
           <Post
             key={item.id}
             {...item}
           />
-        )))}
+        ))}
     </section>
   )
 }
