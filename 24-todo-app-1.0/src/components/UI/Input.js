a5e41513f5db82938cc075cd2d6bfa0c2ad47270
uladzimir <01myit@gmail.com>
2024-01-27 18:37:12 +0300

    Added the controlled state to the Input component.

--- 24-todo-app-1.0/src/components/UI/Input.js
+++ 24-todo-app-1.0/src/components/UI/Input.js
@@ -1,10 +1,17 @@
+import React from 'react'
 import styles from './Input.module.css'
 function Input({ type, placeholder, className }) {
+  const [value, setvalue] = React.useState('')
+  function handleChangeValue(e) {
+    setvalue(e.target.value)
+  }
   return (
     <input
       className={styles[className]}
       type={type}
       placeholder={placeholder}
+      value={value}
+      onChange={handleChangeValue}
     />
   )
 }
