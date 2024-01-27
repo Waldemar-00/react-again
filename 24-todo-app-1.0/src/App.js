142eb0e9cdca1118e5cd8d410c9f830e1e06a53f
uladzimir <01myit@gmail.com>
2024-01-27 16:07:17 +0300

    First commit. Added structure. Created Heading component.

---
+++ 24-todo-app-1.0/src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+import Heading from './components/Header/Heading'
+function App() {
+  return (
+    <div className='App'>
+      <Heading />
+    </div>
+  )
+}
+
+export default App
---
+++ 24-todo-app-1.0/src/components/Header/Heading.js
@@ -0,0 +1,5 @@
+import styles from './Heading.module.css'
+function Heading() {
+  return <h1 className={styles.h1}>Todo App</h1>
+}
+export default Heading
