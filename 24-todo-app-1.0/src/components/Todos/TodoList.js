9348c33bc27e66e9736563d6473bad318e6096c7
uladzimir <01myit@gmail.com>
2024-01-28 16:41:54 +0300

    Added RiTodoFill component from react-icons/ri library and styles for it.

--- 24-todo-app-1.0/src/components/Todos/TodoList.js
+++ 24-todo-app-1.0/src/components/Todos/TodoList.js
@@ -1,3 +1,4 @@
+import { RiTodoFill } from 'react-icons/ri'
 import styles from './TodoList.module.css'
 function TodoList({ notes, handleDeleteNote }) {
   if (!notes.length) return <h2 className={styles.empty}>Todo list is empty</h2>
@@ -8,6 +9,7 @@ function TodoList({ notes, handleDeleteNote }) {
           key={index}
           onDoubleClick={() => handleDeleteNote(index)}
         >
+          <RiTodoFill className={styles.riIcon} />
           {note}
         </li>
       ))}
