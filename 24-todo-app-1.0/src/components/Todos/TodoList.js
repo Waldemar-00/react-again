847eaaf050b4c70800e906324474b4b628cdecc7
uladzimir <01myit@gmail.com>
2024-01-28 15:43:29 +0300

    Added an empty message.

--- 24-todo-app-1.0/src/components/Todos/TodoList.js
+++ 24-todo-app-1.0/src/components/Todos/TodoList.js
@@ -1,5 +1,6 @@
 import styles from './TodoList.module.css'
 function TodoList({ notes, handleDeleteNote }) {
+  if (!notes.length) return <h2 className={styles.empty}>Todo list is empty</h2>
   return (
     <ul className={styles.todoList}>
       {notes.map((note, index) => (
