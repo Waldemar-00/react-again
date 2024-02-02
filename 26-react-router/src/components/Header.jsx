8726664cbc6166498a01ed96c513f112bf3058e5
uladzimir <01myit@gmail.com>
2024-02-02 14:54:33 +0300

    Used NavLink component with className attribute.

--- 26-react-router/src/components/Header.jsx
+++ 26-react-router/src/components/Header.jsx
@@ -2,10 +2,18 @@ import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
     <nav>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About</NavLink>
-      <NavLink to='/about/other'>Other</NavLink>
-      <NavLink to='/contacts'>Contacts</NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : '')} to='/'>
+        Home
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : '')} to='/about'>
+        About
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : '')} to='/about/other'>
+        Other
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : '')} to='/contacts'>
+        Contacts
+      </NavLink>
     </nav>
   )
 }
