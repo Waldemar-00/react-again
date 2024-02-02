6fbc5bf3514ce398dac8babc658af8d18f01de1f
uladzimir <01myit@gmail.com>
2024-02-02 14:43:49 +0300

    Used NavLink component without className attribute.

--- 26-react-router/src/components/Header.jsx
+++ 26-react-router/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/about/other'>Other</Link>
-      <Link to='/contacts'>Contacts</Link>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/about'>About</NavLink>
+      <NavLink to='/about/other'>Other</NavLink>
+      <NavLink to='/contacts'>Contacts</NavLink>
     </nav>
   )
 }
