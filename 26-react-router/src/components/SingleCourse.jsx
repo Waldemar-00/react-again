feca479f172ef2d702c35b5fa0a59e153e4ec79f
uladzimir <01myit@gmail.com>
2024-02-03 14:52:15 +0300

    Used useNavigate hook.

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,15 +1,21 @@
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { courses } from '../data/courses'
-import NotFound404 from './NotFound404'
+// import NotFound404 from './NotFound404'
+import { useEffect } from 'react'
 
 const SingleCourse = () => {
   const { slug } = useParams()
+  const navigate = useNavigate()
   const foundElement = courses.find((course) => course.slug === slug)
-  const content = foundElement ? (
-    foundElement.description
-  ) : (
-    <NotFound404 className='coursesNotfound' text='NOT FOUND COURSE' />
-  )
+  const content = foundElement ? foundElement.description : null
+  useEffect(() => {
+    if (!foundElement) navigate('..', { relative: 'path' }) // or '/courses'
+  }, [foundElement, navigate])
+  // const content = foundElement ? (
+  //   foundElement.description
+  // ) : (
+  //   <NotFound404 className='coursesNotfound' text='NOT FOUND COURSE' />
+  // )
   return (
     <article>
       <div>{content}</div>
