cce6ae53b8baeaeb4d44165366bdbc11f9e742a5
uladzimir <01myit@gmail.com>
2024-02-03 18:39:57 +0300

    Realized sort by the search query of URL.

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,9 +1,11 @@
-import { NavLink, Outlet, useLocation } from 'react-router-dom'
+import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import queryString from 'query-string'
 import { courses } from '../data/courses'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
+const SORT_KEYS = ['id', 'slug', 'title']
 function sortCourses(courses, key) {
+  if (!SORT_KEYS.includes(key)) return courses
   return courses.toSorted((a, b) => (a[key] < b[key] ? -1 : 1))
 }
 const Courses = () => {
@@ -11,9 +13,17 @@ const Courses = () => {
   const query = queryString.parse(location.search)
   const [sortKey, setSortKey] = useState(query.sort)
   const [sortedCourses, setSortedCourses] = useState(sortCourses(courses, sortKey))
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate('.')
+      setSortKey()
+      setSortedCourses([...courses])
+    }
+  }, [navigate, sortKey])
   return (
     <>
-      <h2>Courses</h2>
+      <h2>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h2>
       <ul>
         {sortedCourses.map((course) => {
           return (
