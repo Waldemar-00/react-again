74466c12124ec631fea9c13f1fe9df807fe50e2d
uladzimir <01myit@gmail.com>
2024-02-05 17:38:09 +0300

    Created functions to adding  time and to delete all the  times.

--- 29-redux-pure/index.js
+++ 29-redux-pure/index.js
@@ -2,9 +2,7 @@ import store from './redux/store.js'
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()))
 
-store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
-store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
-unsubscribe()
-store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
-store.dispatch({ type: 'CLEAR_ALL_TIMES' })
-console.log(store.getState())
+document
+  .querySelector('#btnAdd')
+  .addEventListener('click', () => store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() }))
+document.querySelector('#btnDel').addEventListener('click', () => store.dispatch({ type: 'CLEAR_ALL_TIMES' }))
