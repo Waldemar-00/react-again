1b405a65a39818822fd7ebc2ff445906ee5def53
uladzimir <01myit@gmail.com>
2024-02-05 17:13:22 +0300

    Used a fanction unsubscribe.

--- 29-redux-pure/index.js
+++ 29-redux-pure/index.js
@@ -1,8 +1,10 @@
 import store from './redux/store.js'
 
-store.subscribe(() => console.log(store.getState()))
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
 
 store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
 store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
+unsubscribe()
 store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
 store.dispatch({ type: 'CLEAR_ALL_TIMES' })
+console.log(store.getState())
