d48243eda3e7d06a1ba367a3301e13478c8d6840
uladzimir <01myit@gmail.com>
2024-02-05 18:02:56 +0300

    Now you can add time to the page and delete all the times from it.

--- 29-redux-pure/index.js
+++ 29-redux-pure/index.js
@@ -1,8 +1,20 @@
 import store from './redux/store.js'
 
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
+const unsubscribe = store.subscribe(() => {
+  const storeArray = store.getState()
+  storeArray.map((item, index) => {
+    document.querySelector('section').append(document.createElement('div'))
+    document.querySelectorAll('div')[index].innerText = item
+  })
+})
 
 document
   .querySelector('#btnAdd')
   .addEventListener('click', () => store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() }))
-document.querySelector('#btnDel').addEventListener('click', () => store.dispatch({ type: 'CLEAR_ALL_TIMES' }))
+
+document.querySelector('#btnDel').addEventListener('click', () => {
+  store.dispatch({ type: 'CLEAR_ALL_TIMES' })
+  document.querySelectorAll('section div').forEach((item) => {
+    item.parentNode.removeChild(item)
+  })
+})
