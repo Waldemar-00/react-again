c317a5d2720acd29649e2fe1b0d72087b92da510
uladzimir <01myit@gmail.com>
2024-02-05 17:08:33 +0300

    Used a subscribe method from the same object - store.

--- 29-redux-pure/index.js
+++ 29-redux-pure/index.js
@@ -1,8 +1,8 @@
 import store from './redux/store.js'
 
+store.subscribe(() => console.log(store.getState()))
+
 store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
 store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
 store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() })
-console.log(store.getState(), store)
 store.dispatch({ type: 'CLEAR_ALL_TIMES' })
-console.log(store.getState(), store)
