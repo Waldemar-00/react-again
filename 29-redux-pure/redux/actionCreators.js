3ca58f1c8192ac4605ed118956d2dde93bd6749e
uladzimir <01myit@gmail.com>
2024-02-06 17:22:29 +0300

    Created action types.

--- 29-redux-pure/redux/actionCreators.js
+++ 29-redux-pure/redux/actionCreators.js
@@ -1,6 +1,7 @@
+import { ADD_CURRENT_TIME, CLEAR_ALL_TIMES } from './actionTypes.js'
 export const addCurrentTime = (payload) => {
-  return { type: 'ADD_CURRENT_TIME', payload }
+  return { type: ADD_CURRENT_TIME, payload }
 }
 export const clearAllTimes = () => {
-  return { type: 'CLEAR_ALL_TIMES' }
+  return { type: CLEAR_ALL_TIMES }
 }
