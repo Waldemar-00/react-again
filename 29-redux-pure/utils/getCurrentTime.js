21efbd03994a382e6349993312708c9ccb98c045
uladzimir <01myit@gmail.com>
2024-02-06 15:38:08 +0300

    Used padStart method of String.

--- 29-redux-pure/utils/getCurrentTime.js
+++ 29-redux-pure/utils/getCurrentTime.js
@@ -1,11 +1,12 @@
 const getCurrentTime = () => {
   const date = new Date()
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-  const seconds = date.getSeconds()
-  return `${hours < 10 ? '0' + hours : hours} : ${minutes < 10 ? '0' + minutes : minutes} : ${
-    seconds < 10 ? '0' + seconds : seconds
-  }`
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const seconds = date.getSeconds().toString().padStart(2, '0')
+  // return `${hours < 10 ? '0' + hours : hours} : ${minutes < 10 ? '0' + minutes : minutes} : ${
+  //   seconds < 10 ? '0' + seconds : seconds
+  // }`
+  return `${hours} : ${minutes} : ${seconds}`
 }
 
 export default getCurrentTime
