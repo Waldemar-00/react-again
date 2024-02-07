f393695f03d646c578db7924f7fd66d06012f4cc
uladzimir <01myit@gmail.com>
2024-02-07 16:30:47 +0300

    BookForm component changed and added three functions to it

--- 30-react-redux/frontend/src/components/BookForm/BookForm.jsx
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import './BookForm.css'
 const BookForm = () => {
+  const [bookTitle, setBookTitle] = React.useState('')
+  const [author, setAuthor] = React.useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (bookTitle && author) {
+      // dispatch action
+      setBookTitle('')
+      setAuthor('')
+    }
+  }
+  const handleBookTitleChange = (e) => {
+    setBookTitle(e.target.value)
+  }
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value)
+  }
   return (
     <div className='app-block book-form'>
       <h2>Add a new book</h2>
+      <form className='book-form' onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='bookTitle'>Book title:</label>
+          <input id='bookTitle' type='text' value={bookTitle} onChange={handleBookTitleChange} />
+        </div>
+        <div>
+          <label htmlFor='author'>Author:</label>
+          <input id='author' type='text' value={author} onChange={handleAuthorChange} />
+        </div>
+        <button type='submit'>Add Book</button>
+      </form>
     </div>
   )
 }
