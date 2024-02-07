883d9ceda1c31342a656b396feb873078cad4666
uladzimir <01myit@gmail.com>
2024-02-07 14:36:20 +0300

    Structure is ready.

--- 30-react-redux/frontend/src/App.js
+++ 30-react-redux/frontend/src/App.js
@@ -1,5 +1,8 @@
+import React from 'react'
 import './App.css'
-
+import BookForm from './components/BookForm/BookForm'
+import BookList from './components/BookList/BookList'
+import Filter from './components/Filter/Filter'
 function App() {
   return (
     <div className='app'>
