e9ee01a29b1bb623c5905bdc5be9b4f1649822ef
uladzimir <01myit@gmail.com>
2024-02-10 14:36:47 +0300

    Added controled input value

--- 30-react-redux/frontend/src/components/Filter/Filter.jsx
+++ 30-react-redux/frontend/src/components/Filter/Filter.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import filterSlice from '../../redux/slices/filterSlice'
 import './Filter.css'
 const Filter = () => {
+  const dispatch = useDispatch()
+  const title = useSelector((state) => state.filter.title)
   return (
     <div className='app-block filter'>
       <div className='filter-group'>
-        <input type='text' placeholder='Filter by title...' />
+        <input
+          type='text'
+          placeholder='Filter by title...'
+          value={title}
+          onChange={(e) => dispatch(filterSlice.actions.setTitleFilter(e.target.value))}
+        />
       </div>
     </div>
   )
